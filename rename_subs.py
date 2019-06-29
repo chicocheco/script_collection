e89d5d589cbcb474e4165ba78962d628026a04ba
Stanislav Matas <stanislav.matas@gmail.com>
2019-06-29 23:07:39 +0200

    Multiple assignment and membership test used in search for a match.

--- rename_subs.py
+++ rename_subs.py
@@ -4,7 +4,8 @@ from pathlib import Path, PurePath
 from collections import defaultdict
 import pprint
 
-EPISODE_RE = re.compile(r'S\d\dE\d\d', re.IGNORECASE)
+# TODO: mozna nepotrebuju S x E vubec hledat
+EPISODE_RE = re.compile(r'(S)?(\d\d)(x)?(E)?(\d\d)', re.IGNORECASE)
 
 # samples:
 # The.Leftovers.S02E01.HDTV.x264-KILLERS.srt
@@ -14,25 +15,34 @@ EPISODE_RE = re.compile(r'S\d\dE\d\d', re.IGNORECASE)
 dd = defaultdict(dict)
 
 for _, _, files in os.walk('.'):
-    
-    for file in files:
-        # print(file)
-        if file.endswith('.mkv') or file.endswith('.mp4') or file.endswith('.avi'):
-            match = EPISODE_RE.search(file).group().capitalize()
-            dd[match]['video'] = file
 
-        elif file.endswith('.srt'):
-            match = EPISODE_RE.search(file).group().capitalize()
-            # do not override if 'sub' already exists
-            if not dd.get(match, {}).get('sub'):
-                dd[match]['sub'] = file
+    for file in files:
+
+        if file.endswith('.mkv') or file.endswith('.mp4') or file.endswith('.avi'):
+            season, num_season, x, episode, num_episode = EPISODE_RE.search(file).groups()
+            dd[(num_season, num_episode)]['video'] = file  # store video file name
+
+    for file in sorted(files):
+        if file.endswith('.srt'):
+            season, num_season, x, episode, num_episode = EPISODE_RE.search(file).groups()
+            # TODO: check whether a matching subtitles exist already
+
+            if (num_season, num_episode) in dd:
+                if PurePath(dd[(num_season, num_episode)].get('video')).stem == PurePath(file).stem:
+                    dd[(num_season, num_episode)]['sub'] = 'exist'
+                else:
+                    dd[(num_season, num_episode)]['sub'] = file
+            else:
+                print(f'No video found for {file}.')
+
+
 
 # for debugging
 # pprint.pprint(dd)
 
 for value in dd.values():
     if len(value) > 1:
-        if PurePath(value['video']).stem == PurePath(value["sub"]).stem:
+        if value['sub'] == 'exist':  # compare names without its extension
             print(f'\u2713 {value["video"]} already has correctly named subtitles.')
         else:
             video_filename = PurePath(value['video']).stem
@@ -41,10 +51,7 @@ for value in dd.values():
             print(f'{value["sub"]}  >>>  {new_sub}')
             Path(value['sub']).rename(new_sub)
     else:
-        if value['sub']:
-            print(f'\t No video file found for {value["sub"]}')
-        else:
-            print(f'No subtitles found for {value["video"]}')
+        print(f'No subtitles found for {value.get("video")}')
 
 
 
