5bd45d7fce9728c767878a80ec127f10d966682a
Stanislav Matas <stanislav.matas@gmail.com>
2019-06-30 11:36:45 +0200

    Regex covers more patterns now.

--- rename_subs.py
+++ rename_subs.py
@@ -1,16 +1,18 @@
+"""
+Works with the following patterns:
+
+The.Leftovers.S02E01.HDTV.x264-KILLERS.srt
+Colony - 02x10 - The Garden of Beasts.FLEET.English.C.edit.Addic7ed.com.srt
+Falling Skies - Season 5 - Ep. 01 - Find Your Warrior [HDTV.X264]-KiLLeRS.srt
+"""
+
 import os
 import re
 from pathlib import Path, PurePath
 from collections import defaultdict
 import pprint
 
-# TODO: mozna nepotrebuju S x E vubec hledat
-EPISODE_RE = re.compile(r'(S)?(\d\d)(x)?(E)?(\d\d)', re.IGNORECASE)
-
-# samples:
-# The.Leftovers.S02E01.HDTV.x264-KILLERS.srt
-# Falling.Skies.S05E01.HDTV.x264-KILLERS.mp4
-# TODO: Falling Skies - Season 5 - Ep. 01 - Find Your Warrior [HDTV.X264]-KiLLeRS.srt
+EPISODE_RE = re.compile(r'(.?)(\d?\d)', re.IGNORECASE)
 
 dd = defaultdict(dict)
 
@@ -19,25 +21,26 @@ for _, _, files in os.walk('.'):
     for file in files:
 
         if file.endswith('.mkv') or file.endswith('.mp4') or file.endswith('.avi'):
-            season, num_season, x, episode, num_episode = EPISODE_RE.search(file).groups()
-            dd[(num_season, num_episode)]['video'] = file  # store video file name
+            # assuming that the first 2 pairs of digits indicate season + episode
+            (s_lead, season), (e_lead, episode), *rest = EPISODE_RE.findall(file)
+            season = season.zfill(2)
+            episode = episode.zfill(2)
+            dd[(season, episode)]['video'] = file
 
     for file in sorted(files):
+        # sorting is needed to avoid processing new subs when a match had been found already
         if file.endswith('.srt'):
-            season, num_season, x, episode, num_episode = EPISODE_RE.search(file).groups()
-            # TODO: check whether a matching subtitles exist already
-
-            if (num_season, num_episode) in dd:
-                if PurePath(dd[(num_season, num_episode)].get('video')).stem == PurePath(file).stem:
-                    dd[(num_season, num_episode)]['sub'] = 'exist'
+            (s_lead, season), (e_lead, episode), *rest = EPISODE_RE.findall(file)
+            season = season.zfill(2)
+            episode = episode.zfill(2)
+            if (season, episode) in dd:
+                if PurePath(dd[(season, episode)].get('video')).stem == PurePath(file).stem:
+                    dd[(season, episode)]['sub'] = 'exist'
                 else:
-                    dd[(num_season, num_episode)]['sub'] = file
+                    dd[(season, episode)]['sub'] = file
             else:
-                print(f'No video found for {file}.')
+                print(f'No video found for: {file}')
 
-
-
-# for debugging
 # pprint.pprint(dd)
 
 for value in dd.values():
